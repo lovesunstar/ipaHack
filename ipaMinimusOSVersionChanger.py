15fd509fb1106024ea22eb91601d9749d3f5eaab
Dmitry Gambal <xbreeder59x@gmail.com>
2012-07-16 13:41:53 +0300

    - refactorings and improvements; - error handling: now it doesn't throws exceptions in general cases - only readable explanations.

--- ipaMinimusOSVersionChanger.py
+++ ipaMinimusOSVersionChanger.py
@@ -1,67 +1,115 @@
 # -*- coding: utf-8 -*-
 
-import plist, os, sys, shutil, re, zipfile
+import os
+import re
+import sys
+import shutil
+import zipfile
 from pprint import pprint
 
-xml = True
-ipaFileName = None
-ipaFile = None
-plFileName = None
-plFile = None
-binData = None
-dataTree = None
+try:
+	import plist
+except ImportError:
+	raise ImportError(u'python-plist package should be installed in your system.')
 
-if len(sys.argv) == 0:
-	raise ValueError(u'File name should be specified')
-else:
-	ipaFileName = sys.argv[1]
+def extractFile(ipaFileName, errors):	
+	if not zipfile.is_zipfile(os.path.realpath(ipaFileName)) or ipaFileName.find('.ipa') == -1:
+		errors.append(u'Wrong file type or file doesn\'t exist. Must be .ipa')
+		return None
 
-if not zipfile.is_zipfile(os.path.realpath(ipaFileName)):
-	raise ValueError(u'Wrong file type or file doesn\'t exist. Must be .ipa')
+	with zipfile.ZipFile(ipaFileName) as ipaFile:
+		for ipaContent in ipaFile.filelist:
+			if ipaContent.filename.find('Info.plist') > -1:
+				plFileName = ipaFile.extract(ipaContent.filename)
 
-with zipfile.ZipFile(ipaFileName) as ipaFile:
-	for ipaContent in ipaFile.filelist:
-		if ipaContent.filename.find('Info.plist') > -1:
-			plFileName = ipaFile.extract(ipaContent.filename)
+		ipaFile.close()
 
-	if not plFileName:
-		raise IOError(u'File wasn\'t found')
+		if not plFileName:
+			errors.append(u'File wasn\'t found')
+			return None
+		else:
+			return plFileName
 
-with open(plFileName, 'rb') as plFile:
-	binData = plFile.read()
 
-if binData[0] == b'<':
-	dataTree = plist.plist.Structure_from_xml(binData)
-else:
-	dataTree = plist.plist.Structure_from_bin(binData)
-	xml = False
+def buildDataTree(plFileName, errors):
+	with open(plFileName, 'rb') as plFile:
+		binData = plFile.read()
 
-print 'Current OS version requirement: %s' % dataTree['MinimumOSVersion'].get_value()
+	if binData[0] == b'<':
+		dataTree = plist.plist.Structure_from_xml(binData)
+	else:
+		dataTree = plist.plist.Structure_from_bin(binData)
+		xml = False
 
-osVersion = raw_input('Enter new OS Version requirement or leave blank to prevent changes:')
+	if not dataTree['MinimumOSVersion']:
+		errors.append('Minimum OS Version requirement isn\'t setted up. Breaking.')
+		return None
 
-if not re.compile(r"^\d{1}(\.\d){1,2}$").match(osVersion):
-	osVersion = '3.1'
+	return dataTree
 
-dataTree['MinimumOSVersion'].set_value(osVersion)
 
-if xml:
-	binData = dataTree.to_xml()
-else:
-	binData = dataTree.to_bin()
+def changeValueInFile(dataTree, plFileName, errors):
+	print 'Current OS version requirement: %s' % dataTree['MinimumOSVersion'].get_value()
 
-with open(plFileName, 'w+') as plFile:
-	plFile.write(binData)
+	osVersion = raw_input('Enter new OS Version requirement or leave blank to prevent changes:')
 
-length = len(os.getcwd())
-appendFileName = plFileName[length + 1:]
+	if not re.compile(r"^\d{1}(\.\d){1,2}$").match(osVersion):
+		osVersion = '3.1.3'
 
-with zipfile.ZipFile(ipaFileName, 'a') as ipaFile:
-	ipaFile.write(appendFileName)
-	ipaFile.close()
+	dataTree['MinimumOSVersion'].set_value(osVersion)
 
-delimiter = '/' if os.name == 'posix' else '\\'
+	binData = dataTree.to_xml() if xml else dataTree.to_bin()
 
-shutil.rmtree(appendFileName[len(delimiter) - 1:].split(delimiter)[0])
+	try:
+		with open(plFileName, 'w+') as plFile:
+			plFile.write(binData)
+	except (OSError, IOError), e:
+		errors.append(e.__str__())
 
-print 'Done!'
+
+def saveAndRemoveTemp(plFileName, ipaFileName, errors):
+	length = len(os.getcwd())
+	appendFileName = plFileName[length + 1:]
+
+	with zipfile.ZipFile(ipaFileName, 'a') as ipaFile:
+		ipaFile.write(appendFileName)
+		ipaFile.close()
+
+	delimiter = '/' if os.name == 'posix' else '\\'
+
+	shutil.rmtree(appendFileName[len(delimiter) - 1:].split(delimiter)[0])
+
+	print 'Done!'
+	
+
+def Do(name, errors):
+	xml = True
+	ipaFileName = ''
+
+	plFileName = extractFile(sys.argv[1], errors)
+
+	if not len(errors):
+		dataTree = buildDataTree(plFileName, errors)
+	else:
+		return None
+
+	if not len(errors):
+		changeValueInFile(dataTree, plFileName, errors)
+	else:
+		return None
+
+	if not len(errors):
+		saveAndRemoveTemp(plFileName, ipaFileName, errors)
+		return True
+	else:
+		return None	
+
+if __name__ == "__main__":
+	errors = []
+
+	if len(sys.argv) < 2:
+		errors.append(u'File name should be specified')
+		pprint(errors)
+	else:
+		if not Do(sys.argv[1], errors):
+			pprint(errors)
